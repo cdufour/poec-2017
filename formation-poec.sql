eb8f5ef4927524e27483696a1de6825dab6071bf
cdufour <opusidea@gmail.com>
2017-05-24 17:36:14 +0200

    jour 6

--- formation-poec.sql
+++ formation-poec.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Client :  127.0.0.1
--- Généré le :  Mar 23 Mai 2017 à 13:42
+-- Généré le :  Mer 24 Mai 2017 à 15:34
 -- Version du serveur :  5.7.14
 -- Version de PHP :  5.6.25
 
@@ -30,17 +30,21 @@ CREATE TABLE `equipe` (
   `id` int(11) NOT NULL,
   `nom` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `entraineur` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `couleurs` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `couleurs` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `logo` varchar(255) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Contenu de la table `equipe`
 --
 
-INSERT INTO `equipe` (`id`, `nom`, `entraineur`, `couleurs`) VALUES
-(1, 'Juventus', 'Massimiliano Allegri', 'noir, blanc'),
-(2, 'Strasbourg', 'Jean-Pierre Papin', 'bleu, blanc'),
-(3, 'PSG', 'Laurent Blanc', 'rouge, bleu');
+INSERT INTO `equipe` (`id`, `nom`, `entraineur`, `couleurs`, `logo`) VALUES
+(1, 'Juventus', 'Massimiliano Allegri', 'noir, blanc', ''),
+(2, 'Strasbourg', 'Jean-Pierre Papin', 'bleu, blanc', ''),
+(3, 'PSG', 'Laurent Blanc', 'rouge, bleu', ''),
+(4, 'ASNL', 'Correa', 'blanc, rouge', ''),
+(5, 'Benfica', 'Rui Costa', 'Vert, rouge', 'img/logo/Benfica.jpg'),
+(6, 'Monaco', 'Jardim', 'blanc, rouge', 'img/logo/Monaco.png');
 
 -- --------------------------------------------------------
 
@@ -63,14 +67,14 @@ CREATE TABLE `joueur` (
 
 INSERT INTO `joueur` (`id`, `nom`, `prenom`, `age`, `numero_maillot`, `equipe`) VALUES
 (1, 'Bonucci', 'Leonardo', 29, 0, 0),
-(3, 'Ronaldo', 'Cristiano', 32, 0, 0),
+(3, 'Ronaldo', 'Cristiano', 32, 1, 3),
 (4, 'Pogba', 'Paul', 23, 10, 0),
-(5, 'Cantona', 'Eric', 50, 0, 0),
 (6, 'Marchisio', 'Claudio', 33, 18, 0),
 (10, 'Gameiro', 'KÃ©vin', 28, 11, 0),
 (11, 'Griezman', 'Antoine', 26, 7, 0),
 (12, 'Dybala', 'Paolo', 23, 21, 2),
-(13, 'Cavani', 'Edinson', 32, 9, 2);
+(13, 'Cavani', 'Edinson', 32, 9, 2),
+(14, 'Falcao', 'Radamel', 34, 9, 6);
 
 --
 -- Index pour les tables exportées
@@ -96,12 +100,12 @@ ALTER TABLE `joueur`
 -- AUTO_INCREMENT pour la table `equipe`
 --
 ALTER TABLE `equipe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `joueur`
 --
 ALTER TABLE `joueur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
