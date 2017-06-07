f04e68b1bf1b678632fec45677b144247e591b93
cdufour <opusidea@gmail.com>
2017-06-07 13:11:00 +0200

    jour 12 TP optimisation

--- TP/selectCountry/js/app.js
+++ TP/selectCountry/js/app.js
@@ -1,15 +1,41 @@
+var pays_infos = []; // tableau (vide au départ) permettant de mettre
+// en cache le retour des requêtes ajax
+
 $('select').on('change', function() {
     var option = $(this).val();
 
     // si pays sélectionné => requête ajax
     if (option > 0) {
-        var url = 'http://localhost/projet/php/TP/selectCountry/pays.php?id='+option;
-        $.get(url, function(data) {
-            // affichage des données dans la page
-            var pays = JSON.parse(data); // conversion de la chaîne JSON
-            // en tableau JS
-            displayCountryData(pays);
-        });
+        // on affiche la fiche pays
+        $('#pays_infos').show();
+
+        // index_pays vaudra -1 si le pays n'a pas été trouvé dans pays_infos
+        // sinon index_pays vaudra l'indice du pays trouvé dans le tableau
+        var index_pays = isCountryInArray(pays_infos, option);
+
+        if (index_pays != -1) {
+            // pays déjà présent dans le tableau
+            displayCountryData(pays_infos[index_pays]);
+        } else {
+            // pays non présent => requête ajax
+            var url = 'http://localhost/projet/php/TP/selectCountry/pays.php?id='+option;
+            $.get(url, function(data) {
+                // affichage des données dans la page
+                var pays = JSON.parse(data); // conversion de la chaîne JSON
+                // en tableau JS
+                displayCountryData(pays);
+
+                // mise en cache des données
+                pays_infos.push(pays);
+                console.log(pays_infos);
+            });
+        }
+
+
+
+
+    } else {
+        $('#pays_infos').hide(); // masque la zone d'infos pays
     }
 
     function displayCountryData(country) {
@@ -20,5 +46,17 @@ $('select').on('change', function() {
         spans.eq(3).text(country.langues);
 
         $('img.flag').attr('src', country.drapeau);
+    }
+
+    function isCountryInArray(arr, id) {
+        var found = -1; // -1 signifie "aucun indice correspondant"
+
+        arr.forEach(function(item, index) {
+            if (item.id == id) {
+                found = index ; // pays recherché trouvé !
+            }
+        });
+
+        return found; // renvoie l'indice du pays trouvé
     }
 });
--- TP/selectCountry/pays.php
+++ TP/selectCountry/pays.php
@@ -3,6 +3,10 @@ include '../../includes/connexion_db.php';
 
 $id = $_GET['id']; // récupération de l'id fourni en paramètre d'url
 $db = connect();
+
+$db->exec("set names utf8"); // résoud le problème de l'encodage en utf8 (prélude nécessaire à l'encodage JSON)
+// https://openclassrooms.com/forum/sujet/encodage-de-caracteres-lors-d-une-requete-sql
+
 $query = $db->prepare('SELECT * FROM pays WHERE id = :id');
 $query->execute(array(
     ':id' => $id
@@ -10,6 +14,8 @@ $query->execute(array(
 $pays = $query->fetch();
 
 // réponse au client
+
+// utf8_encode() ne résoud pas le problème
 echo json_encode($pays);
 
 ?>
